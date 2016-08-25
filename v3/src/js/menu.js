72d698b57bb2b0d6f9492f3bf11308a50e9f8517
Patryk Sadza <exihuatl@gmail.com>
2016-08-25 14:09:50 +0200

    s

--- v3/src/js/menu.js
+++ v3/src/js/menu.js
@@ -1,134 +1,140 @@
-$(document).ready(function(){
-	var menuItemNum=$(".menu-item").length;
-	var angle=180;
-	var distance=90;
-	var startingAngle=270;
-	var slice=angle/(menuItemNum-1);
-	TweenMax.globalTimeScale(0.8);
-	$(".menu-item").each(function(i){
-		var angle=startingAngle+(slice*i);
-		$(this).css({
-			transform:"rotate("+(angle)+"deg)"
-		})
-		$(this).find(".menu-item-icon").css({
-			transform:"rotate("+(-angle)+"deg)"
-		})
-	})
-	var on=false;
+/* global TweenMax */
+/* global Quint */
+/* global Quad */
+/* global Elastic */
 
-	$(".menu-toggle-button").mousedown(function(){
-		TweenMax.to($(".menu-toggle-icon"),0.1,{
-			scale:0.65
-		})
-	})
-	$(document).mouseup(function(){
-		TweenMax.to($(".menu-toggle-icon"),0.1,{
-			scale:1
-		})
-	});
-	$(document).on("touchend",function(){
-		$(document).trigger("mouseup")
-	})
-	$(".menu-toggle-button").on("mousedown",pressHandler);
-	$(".menu-toggle-button").on("touchstart",function(event){
-		$(this).trigger("mousedown");
-		event.preventDefault();
-		event.stopPropagation();
-	});
+const menuItems = document.querySelectorAll('.menu-item');
+const menuItemNum = menuItems.length;
+const angle = 180;
+const distance = 90;
+const startingAngle = 270;
+const slice = angle / (menuItemNum - 1);
+TweenMax.globalTimeScale(0.8);
 
-	function pressHandler(event){
-		on=!on;
+menuItems.forEach((el, i) => {
+  const currentAngle = startingAngle + (slice * i);
+  const element = el;
 
-		TweenMax.to($(this).children('.menu-toggle-icon'),0.4,{
-			rotation:on?45:0,
-			ease:Quint.easeInOut,
-			force3D:true
-		});
+  element.style.transform = `rotate(${currentAngle}deg)`;
+  element.querySelector('.menu-item-icon').style.transform = `rotate${currentAngle}deg`;
+});
 
-		on?openMenu():closeMenu();
+let on = false;
 
-	}
-	function openMenu(){
-		$(".menu-item").each(function(i){
-			var delay=i*0.08;
+document.querySelector('.menu-toggle-button').addEventListener('mousedown', () => {
+  TweenMax.to(document.querySelector('.menu-toggle-icon'), 0.1, {
+    scale: 0.65,
+  });
+});
 
-			var $bounce=$(this).children(".menu-item-bounce");
-
-			TweenMax.fromTo($bounce,0.2,{
-				transformOrigin:"50% 50%"
-			},{
-				delay:delay,
-				scaleX:0.8,
-				scaleY:1.2,
-				force3D:true,
-				ease:Quad.easeInOut,
-				onComplete:function(){
-					TweenMax.to($bounce,0.15,{
-						// scaleX:1.2,
-						scaleY:0.7,
-						force3D:true,
-						ease:Quad.easeInOut,
-						onComplete:function(){
-							TweenMax.to($bounce,3,{
-								// scaleX:1,
-								scaleY:0.8,
-								force3D:true,
-								ease:Elastic.easeOut,
-								easeParams:[1.1,0.12]
-							})
-						}
-					})
-				}
-			});
-
-			TweenMax.to($(this).children(".menu-item-button"),0.5,{
-				delay:delay,
-				y:distance,
-				force3D:true,
-				ease:Quint.easeInOut
-			});
-		})
-	}
-	function closeMenu(){
-		$(".menu-item").each(function(i){
-			var delay=i*0.08;
-
-			var $bounce=$(this).children(".menu-item-bounce");
-
-			TweenMax.fromTo($bounce,0.2,{
-				transformOrigin:"50% 50%"
-			},{
-				delay:delay,
-				scaleX:1,
-				scaleY:0.8,
-				force3D:true,
-				ease:Quad.easeInOut,
-				onComplete:function(){
-					TweenMax.to($bounce,0.15,{
-						// scaleX:1.2,
-						scaleY:1.2,
-						force3D:true,
-						ease:Quad.easeInOut,
-						onComplete:function(){
-							TweenMax.to($bounce,3,{
-								// scaleX:1,
-								scaleY:1,
-								force3D:true,
-								ease:Elastic.easeOut,
-								easeParams:[1.1,0.12]
-							})
-						}
-					})
-				}
-			});
+document.addEventListener('mouseup', () => {
+  TweenMax.to(document.querySelector('.menu-toggle-icon'), 0.1, {
+    scale: 1,
+  });
+});
 
 
-			TweenMax.to($(this).children(".menu-item-button"),0.3,{
-				delay:delay,
-				y:0,
-				force3D:true,
-				ease:Quint.easeIn
-			});
-		})
-	}
-})
+function openMenu() {
+  document.querySelectorAll('.menu-item').forEach((el, i) => {
+    const delay = i * 0.08;
+
+    const bounce = el.querySelectorAll('.menu-item-bounce');
+
+    TweenMax.fromTo(bounce, 0.2, {
+      transformOrigin: '50% 50%',
+    }, {
+      delay,
+      scaleX: 0.8,
+      scaleY: 1.2,
+      force3D: true,
+      ease: Quad.easeInOut,
+      onComplete() {
+        TweenMax.to(bounce, 0.15, {
+          // scaleX:1.2,
+          scaleY: 0.7,
+          force3D: true,
+          ease: Quad.easeInOut,
+          onComplete() {
+            TweenMax.to(bounce, 3, {
+              // scaleX:1,
+              scaleY: 0.8,
+              force3D: true,
+              ease: Elastic.easeOut,
+              easeParams: [1.1, 0.12],
+            });
+          },
+        });
+      },
+    });
+
+    TweenMax.to(el.querySelector('.menu-item-button'), 0.5, {
+      delay,
+      y: distance,
+      force3D: true,
+      ease: Quint.easeInOut,
+    });
+  });
+}
+
+function closeMenu() {
+  document.querySelectorAll('.menu-item').forEach((el, i) => {
+    const delay = i * 0.08;
+
+    const bounce = el.querySelectorAll('.menu-item-bounce');
+
+    TweenMax.fromTo(bounce, 0.2, {
+      transformOrigin: '50% 50%',
+    }, {
+      delay,
+      scaleX: 1,
+      scaleY: 0.8,
+      force3D: true,
+      ease: Quad.easeInOut,
+      onComplete() {
+        TweenMax.to(bounce, 0.15, {
+          // scaleX:1.2,
+          scaleY: 1.2,
+          force3D: true,
+          ease: Quad.easeInOut,
+          onComplete() {
+            TweenMax.to(bounce, 3, {
+              // scaleX:1,
+              scaleY: 1,
+              force3D: true,
+              ease: Elastic.easeOut,
+              easeParams: [1.1, 0.12],
+            });
+          },
+        });
+      },
+    });
+
+
+    TweenMax.to(el.querySelector('.menu-item-button'), 0.3, {
+      delay,
+      y: 0,
+      force3D: true,
+      ease: Quint.easeIn,
+    });
+  });
+}
+
+
+function pressHandler() {
+  on = !on;
+
+  TweenMax.to(document.querySelectorAll('.menu-toggle-icon'), 0.4, {
+    rotation: on ? 45 : 0,
+    ease: Quint.easeInOut,
+    force3D: true,
+  });
+
+  if (on) {
+    openMenu();
+  } else {
+    closeMenu();
+  }
+}
+
+document.querySelector('.menu-toggle-button').addEventListener('mousedown', pressHandler);
+
